0bab8f1ddb8af300ef1280636f08fd195edc2045
Nahid Sayyed <nahidnsayyed786@gmail.com>
2026-03-27 00:57:02 +0530

    Fix MongoDB connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/bloodbank");
-    console.log("Database connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error(error);
+    console.error("MongoDB error:", error);
     process.exit(1);
   }
 };
